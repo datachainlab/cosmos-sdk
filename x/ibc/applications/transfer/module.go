b5c1fb2ebb3e4a2c574f421f372e6f5962f0c4d6
Jun Kimura <junkxdev@gmail.com>
2021-01-18 17:47:08 +0900

    fix ack format

--- x/ibc/applications/transfer/module.go
+++ x/ibc/applications/transfer/module.go
@@ -360,7 +360,7 @@ func (am AppModule) OnAcknowledgementPacket(
 			sdk.NewAttribute(types.AttributeKeyReceiver, data.Receiver),
 			sdk.NewAttribute(types.AttributeKeyDenom, data.Denom),
 			sdk.NewAttribute(types.AttributeKeyAmount, fmt.Sprintf("%d", data.Amount)),
-			sdk.NewAttribute(types.AttributeKeyAck, fmt.Sprintf("%v", ack)),
+			sdk.NewAttribute(types.AttributeKeyAck, fmt.Sprintf("%v", ack.String())),
 		),
 	)
 
